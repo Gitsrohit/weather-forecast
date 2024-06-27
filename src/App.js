3f275087ce7397965b71b7f34275a606761651f4
Gitsrohit <thissiderohit11@gmail.com>
2024-06-27 22:42:58 +0530

    forcast

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import CurrentLocation from "./currentLocation";
 import "./App.css";
+import logo from './images/logo.png';
 
 function App() {
   return (
     <React.Fragment>
+      <img className="logoo" src={logo} alt="Logo" />
       <div className="container">
         <CurrentLocation />
       </div>
